e134ce302b63a9b807dff8fc200150364f976430
bryanfowler25 <47465794+bryanfowler25@users.noreply.github.com>
2019-09-25 21:19:51 -0400

    9:19PM

--- hw1/Calculator/src/app/home/home.page.ts
+++ hw1/Calculator/src/app/home/home.page.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public val1: string;
+  public val2: string;
+  public op: string;
   constructor() {}
+  calculate() {
+    const a = +this.val1;
+    const b = +this.val2;
+
+    
+  }
 
 }
